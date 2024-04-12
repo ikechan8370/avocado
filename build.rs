59df45022e4bd70aa78f7c561617863b13d9ea27
ikechan8370 <geyinchi@buaa.edu.cn>
2024-04-12 14:48:28 +0800

    feat: support dynamic plugin mod loading

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::io::Write;
 use std::path::{Path, PathBuf};
 
 fn collect_proto_files<P: AsRef<Path>>(dir: P) -> Vec<PathBuf> {
@@ -19,6 +20,10 @@ fn collect_proto_files<P: AsRef<Path>>(dir: P) -> Vec<PathBuf> {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    println!("cargo:rerun-if-changed=kritor/protos");
+    println!("cargo:rerun-if-changed=src/service/plugins");
+
     let proto_files = collect_proto_files("kritor/protos");
 
     let proto_files_str = proto_files.iter()
@@ -29,6 +34,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_server(true)
         // .proto_path("kritor/protos")
         .compile(&proto_files_str, &["kritor/protos"])?;
+
+
+    // 加载插件
+    let out_dir = "src/service/plugins";
+    let dest_path = Path::new(&out_dir).join("mod.rs");
+    let mut f = fs::File::create(&dest_path).unwrap();
+
+    let paths = fs::read_dir(out_dir).unwrap();
+
+    for path in paths {
+        let path = path.unwrap().path();
+        if path.is_dir() {
+            if let Some(os_str) = path.file_name() {
+                if let Some(dir_name) = os_str.to_str() {
+                    writeln!(f, "mod {};", dir_name).unwrap();
+                }
+            }
+        } else if let Some(extension) = path.extension() {
+            if extension == "rs" {
+                if let Some(os_str) = path.file_stem() {
+                    if let Some(file_name) = os_str.to_str() {
+                        if file_name != "mod" && file_name != "main" {
+                            writeln!(f, "mod {};", file_name).unwrap();
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     Ok(())
 }
